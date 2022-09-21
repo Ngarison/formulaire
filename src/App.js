d6a9b37bfa4ee0667d37408e9b8a5c4118b35061
Ngarison <jules.ngari@gmail.com>
2022-09-21 03:50:24 +0100

    ajout du formulaire

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+            <form>
+        <div class="row mb-3">
+          <label for="inputEmail3" class="col-sm-4 col-form-label">Email</label>
+          <div class="col-sm-10">
+            <input type="email" class="form-control" id="inputEmail3"/>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <label for="inputPassword3" class="col-sm-4 col-form-label">Password</label>
+          <div class="col-sm-10">
+            <input type="password" class="form-control" id="inputPassword3"/>
+          </div>
+        </div>
+        <div class="col-6 col-sm-3">
+        <button type="submit" class="btn btn-primary">Sign in</button>
+        </div>
+      </form>
       </header>
     </div>
   );
